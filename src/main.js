cc20a64dfffbb7ec53b40349ab93887a391808aa
antHome <antaNT.pro@gmail.com>
2025-02-23 13:36:38 +0300

    vsc

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 import { mount } from 'svelte'
 import App from './views/App.svelte'
 
-mount(App, { target: document.body, props: { name: 'World' } })
+mount(App, { target: document.body, props: { name: 'antWorld' } })
